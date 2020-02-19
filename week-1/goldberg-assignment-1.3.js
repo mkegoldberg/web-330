7c82d32ab8abb798a4faf1cebbaed9fe51874dee
Mike Goldberg <mkegoldberg@yahoo.com>
2020-02-18 23:13:01 -0800

    adding assignment 1.3

---
+++ week-1/goldberg-assignment-1.3.js
@@ -0,0 +1,53 @@
+/*
+============================================
+; Title:  object-literal-class.js
+; Author: Professor Krasso
+; Date:   28 February 2019
+; Description: Refresher on object literal classes
+;===========================================
+*/
+
+// Require statement that imports the header.js file from my root directory.
+const header = require('../goldberg-header.js');
+
+/*
+ Expected output:
+
+-- DISPLAYING CELL PHONE DETAILS --
+ Manufacturer: blackberry
+ Model: z20
+ Color: purple
+ Price: $159
+
+*/
+
+// start program
+
+// variable declaration
+function CellPhone(manufacturer, model, color, price) {
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.color = color;
+    this.price = price;
+
+    this.getPrice = function () {
+        return this.price;
+    };
+    this.getModel = function () {
+        return this.model;
+    };
+    this.getDetails = function() {
+        return "Manufacturer: " + this.manufacturer + "\n" + "Model: " + this.getModel() + "\n" + "Color: " + this.color + "\n" + "Price: $" + this.getPrice();
+    };
+
+}
+
+var phone1 = new CellPhone("blackberry", "z20", "purple", "159");
+
+// output
+console.log(header.display("Mike", "Goldberg", "assignment 1.3"), "\n");
+
+console.log("-- DISPLAYING CELL PHONE DETAILS --")
+console.log(phone1.getDetails());
+
+// end program
